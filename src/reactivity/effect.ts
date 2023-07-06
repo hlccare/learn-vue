26edbc494b38a9dadabf302d0f2dd7922a5f737a
hlccare <280108904@qq.com>
2023-07-06 17:16:10 +0800

    fix: 修复stop后get触发依赖收集的问题

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
 import { extend } from "../shared";
 
+// 全局变量，用来标识当前执行的effect
+let activeEffect;
+// shouldTrack用于控制 fn 执行时，其中的响应式对象是否收集依赖，默认不收集
+let shouldTrack;
+
 class ReactiveEffect {
   private _fn: any;
   onStop?: () => void;
@@ -11,9 +16,20 @@ class ReactiveEffect {
     this.scheduler = scheduler;
   }
   run() {
-    // 调用run，修改activeEffect指向，并调用fn
-    activeEffect = this;
-    return this._fn();
+    if (!this.active) {
+      return this._fn();
+    }
+
+    // active为true，则允许进行依赖收集
+    shouldTrack = true;
+    activeEffect = this; // 调用run，修改activeEffect指向，并调用fn
+
+    const result = this._fn();
+
+    // 执行完 fn 后，重置shouldTrack为false
+    shouldTrack = false;
+
+    return result;
   }
   stop() {
     if (this.active) {
@@ -33,6 +49,8 @@ function cleanupEffect(effect: ReactiveEffect) {
 // targetMap，以目标对象为健，值为一个depsMap
 const targetMap = new Map();
 export function track(target, key) {
+  if (!isTracking()) return;
+
   // target -> key -> dep
   // depsMap，以key为键，值为dep，effect的集合
   let depsMap = targetMap.get(target);
@@ -46,9 +64,13 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
-  if (!activeEffect) return;
+  if (dep.has(activeEffect)) return; // 避免重复收集
   dep.add(activeEffect);
   activeEffect.deps.push(dep);
+}
+
+function isTracking() {
+  return shouldTrack && activeEffect !== undefined;
 }
 
 export function trigger(target, key) {
@@ -65,8 +87,6 @@ export function trigger(target, key) {
   }
 }
 
-// 全局变量，用来标识当前执行的effect
-let activeEffect;
 export function effect(fn, options?: { scheduler?; onStop? }) {
   const _effect = new ReactiveEffect(fn, options?.scheduler);
   // extend
