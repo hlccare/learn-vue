c156a99fa2fa178c77e0bf7a71629587c74bd33c
hlccare <280108904@qq.com>
2023-07-06 12:19:56 +0800

    feat: reactive增加isReactive\isReadonly

--- src/reactivity/baseHandlers.ts
+++ src/reactivity/baseHandlers.ts
@@ -1,4 +1,5 @@
 import { track, trigger } from "./effect";
+import { ReactiveFlags, readonly } from "./reactive";
 
 // 复用，避免重复创建
 const get = createGetter();
@@ -8,6 +9,12 @@ const readonlyGet = createGetter(true);
 function createGetter(isReadonly = false) {
   return function get(target, key) {
     const res = Reflect.get(target, key);
+
+    if (key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if (key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
 
     if (!isReadonly) {
       track(target, key);
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,11 +1,26 @@
 import { mutableHandlers, readonlyHandlers } from "./baseHandlers";
 
+export const enum ReactiveFlags {
+  IS_REACTIVE = "__v_isReactive",
+  IS_READONLY = "__v_isReadonly",
+}
+
 export function reactive(raw) {
   return createActiveObject(raw, mutableHandlers);
 }
 
 export function readonly(raw) {
   return createActiveObject(raw, readonlyHandlers);
+}
+
+export function isReactive(value) {
+  // !!用于将undefined转换为布尔值false
+  return !!value[ReactiveFlags.IS_REACTIVE];
+}
+
+export function isReadonly(value) {
+  // !!用于将undefined转换为布尔值false
+  return !!value[ReactiveFlags.IS_READONLY];
 }
 
 export function createActiveObject(raw, baseHandlers) {
